89e085a8f33fb055b5984e7dcc89abb8a543140a
Nathan Walker <walkerrunpdx@gmail.com>
2022-07-05 17:39:21 -0700

    feat: update angular 14

--- examples/nativescript-ng/references.d.ts
+++ examples/nativescript-ng/references.d.ts
@@ -1,3 +1,2 @@
-/* eslint-disable */
 /// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
